51cf51d1630c04fd66d64179a2f82b5644c42a1e
Sam Jones <samakj@live.co.uk>
2020-05-28 11:57:04 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="midge",
+    version="0.1",
+    description="A small paho.mqtt wrapper",
+    url="http://github.com/samakj/midge",
+    author="Sam Jones",
+    author_email="samakj@live.co.uk",
+    license="MIT",
+    packages=["midge"],
+    install_requires=["paho-mqtt"],
+    zip_safe=False
+)
